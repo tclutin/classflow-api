960ab116bf64895cee2c37927651a2f291290c53
Lutin <104318173+tclutin@users.noreply.github.com>
2025-04-07 22:23:09 +0500

    feat: new metric

--- internal/metric/metric.go
+++ internal/metric/metric.go
@@ -14,10 +14,25 @@ var httpRequestsCounter = prometheus.NewCounterVec(
 	[]string{"endpoint"},
 )
 
+var scheduleRequestCounter = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Namespace: "classflow_api",
+		Subsystem: "http",
+		Name:      "schedule_request_total",
+		Help:      "Total HTTP requests by path and group",
+	},
+	[]string{"endpoint", "group_name"},
+)
+
 func init() {
 	prometheus.MustRegister(httpRequestsCounter)
+	prometheus.MustRegister(scheduleRequestCounter)
 }
 
 func IncRequestCounter(endpoint string) {
 	httpRequestsCounter.WithLabelValues(endpoint).Inc()
+}
+
+func IncScheduleRequestCounter(endpoint, groupName string) {
+	scheduleRequestCounter.WithLabelValues(endpoint, groupName).Inc()
 }
